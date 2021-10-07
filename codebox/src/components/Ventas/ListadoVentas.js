dca5f44228b1241a259838069e7a02cf625c7947
admontero <andresdavidmonterofernandez@gmail.com>
2021-10-06 19:09:56 -0500

    Ajustes del css

--- codebox/src/components/Ventas/ListadoVentas.js
+++ codebox/src/components/Ventas/ListadoVentas.js
@@ -18,7 +18,7 @@ const ListadoVentas = () => {
                         setShowAlert = { setShowAlert }
                     />
                 :
-                ''
+                    null
             }
             <section className="main-container">
                 <div className="cards">
@@ -71,9 +71,8 @@ const ListadoVentas = () => {
                                         <td>Tiendas ARA</td>
                                         <td>
                                             <span className="tag-status cancelled">cancelada</span>
-                                            
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -82,7 +81,6 @@ const ListadoVentas = () => {
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                                             </button>
                                         </td>
-
                                     </tr>
                                     <tr>
                                         <td>2</td>
@@ -91,7 +89,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status paid">pagada</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -108,7 +106,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status pending">pendiente</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -125,7 +123,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status paid">pagada</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -142,7 +140,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status cancelled">cancelada</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -159,7 +157,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status pending">pendiente</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -176,7 +174,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status pending">pendiente</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
@@ -193,7 +191,7 @@ const ListadoVentas = () => {
                                         <td>
                                             <span className="tag-status paid">pagada</span>
                                         </td>
-                                        <td id = "acction">
+                                        <td className="action">
                                             <Link to="/ventas/editar" className="editar">
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                 
